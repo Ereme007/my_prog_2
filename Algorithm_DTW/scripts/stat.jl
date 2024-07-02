c16d631e64378c71d0c3a02c98d375124e97c39a
Елизавета Александровна Еременко <eremenko.liz@mail.ru>
2024-07-02 18:43:11 +0300

    Statisctics

--- Algorithm_DTW/scripts/stat.jl
+++ Algorithm_DTW/scripts/stat.jl
@@ -11,11 +11,12 @@ using JLD2, Plots
 plotly()
 #По одному шаблону на каждый класс, без изменений (CTS)
 @load "Algorithm_DTW/src/Templates/templates_CTS.jld2" Templates_CTS_Q Templates_CTS_QR Templates_CTS_QRS Templates_CTS_RS Templates_CTS_RSR Templates_CTS_R
-#По одному шаблону на каждый класс с одинаковым размахом
+#По одному шаблону на каждый класс с одинаковым размахом, начинающиеся с нулевого уровня
 @load "Algorithm_DTW/src/Templates/templates_CTS_scope.jld2" scope_Templates_CTS_Q scope_Templates_CTS_QR scope_Templates_CTS_QRS scope_Templates_CTS_RS scope_Templates_CTS_RSR scope_Templates_CTS_R #Это не обязательно, достаточно применить 2 функции scope(Zeros_signal("TEMPLATES"))
-#Несколько шаблонов на каждый класс(неравномерное распределение шаблонов по классу) с одинаковым размахом
+#Несколько шаблонов на каждый класс(неравномерное распределение шаблонов по классу) с одинаковым размахом, начинающиеся с нулевого уровня
 @load "Algorithm_DTW/src/Templates/scope_More_Templates_CTS.jld2" scope_More_Templates_CTS_Q scope_More_Templates_CTS_QR scope_More_Templates_CTS_QRS scope_More_Templates_CTS_RS scope_More_Templates_CTS_RSR scope_More_Templates_CTS_R
 
+#Берём за шаблоны, которых модет быть несколько в каждом классе, имеющий одинаковый размах, начинающиеся с нулевого уровня.
 Templates_Q = scope_More_Templates_CTS_Q
 Templates_QR = scope_More_Templates_CTS_QR
 Templates_QRS = scope_More_Templates_CTS_QRS
@@ -26,13 +27,26 @@ Templates_R = scope_More_Templates_CTS_R
 #Отрисовка шаблонов
 pl.plot_templates(Templates_Q, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR, Templates_R)
 
-#Отрисовка сигнала с сопастовлением для него класса
-BaseName, N = "CSE", 2
+##Отрисовка сигнала с сопостовлением для него класса
+#Определяем базу и номер сигнала
+BaseName, N = "CSE", 2 #имеем базы "CSE" и "CTS"
+
+#Определяем сигнал
 Names_files, signals_channel, Frequency, Ref_qrs = rd.Signal_all_channels(BaseName, N)
 
-Channel, K = 1, 6
-Signal = rd.scope(rd.Zeros_signal(signals_channel[Channel][Ref_qrs[1]:Ref_qrs[2]]))
+Channel, K = 1, 6 #номер отведения и k-бижайших соседей для оценки
+
+#Сигнал БЕЗ преобразований
+Signal = signals_channel[Channel][Ref_qrs[1]:Ref_qrs[2]]
+
+#Сигнал С преобразованями
+Pr_Signal = rd.Processing_Signal(Signal)
+
+#Отрисовка сиганла с сопостовлением для него класса
 pl.plots_result(K, Signal, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR)
 
-#Изначальный сегмент сигнала (без обработки)
-plot(signals_channel[Channel][Ref_qrs[1]:Ref_qrs[2]])
+#Изначальный сегмент сигнала БЕЗ обработки
+plot(Signal)
+
+#Сохраняем статистику (определён CSE база для 60ти сигналов по 12 отведений)
+pl.Save_csv("Test1", K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR)
--- Algorithm_DTW/src/Plotting.jl
+++ Algorithm_DTW/src/Plotting.jl
@@ -1,8 +1,11 @@
 module Plotting
     include("DTWfunc.jl")
     import .DTWfunc as dtw
-
+    include("../src/Readers.jl")
+    import .Readers as rd
+    
     using Plots
+    using CSV, Tables, DataFrames
     
     #Q, R, QR, QRS, RS, RSR
     function plot_templates(Q, R, QR, QRS, RS, RSR)
@@ -17,8 +20,116 @@ module Plotting
     end
 
     function plots_result(K, Sig, Q, R, QR, QRS, RS, RSR)
-        Res, _ = dtw.Result_DTW(K, Sig, Q, R, QR, QRS, RS, RSR)
+        Res, _ = dtw.Result_DTW(Sig, K, Q, R, QR, QRS, RS, RSR)
         plot(Sig, legend = false, title = Res)
     end
-    export plot_templates, plots_result
+
+
+
+
+
+    function Save_csv(name, K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR)
+
+        Mass_Names_files = []
+        First_ch = []
+        Second_ch = []
+        Third_ch = []
+        Four_ch = []
+        Five_ch = []
+        Six_ch = []
+        Seven_ch = []
+        Eight_ch = []
+        Nine_ch = []
+        Ten_ch = []
+        Eleven_ch = []
+        Twelve_ch = []
+       # K = 6
+       BaseName = "CSE"
+        for i in 1:60
+        NUMBER = i
+        Names_files, const_signal,  Frequency, Ref_qrs = rd.Signal_all_channels(BaseName, NUMBER)
+        QRS_start = Ref_qrs[1]
+        QRS_end = Ref_qrs[2]
+        push!(Mass_Names_files, Names_files)
+    
+        Signal = const_signal[1][QRS_start:QRS_end]
+        #L1 = Return_only_complex(const_signal, 1)
+        L1, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR)
+        push!(First_ch, L1)
+        Signal = const_signal[2][QRS_start:QRS_end]
+        #L2 = Return_only_complex(const_signal, 2)
+        L2, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR)
+        push!(Second_ch, L2)
+    
+        Signal = const_signal[3][QRS_start:QRS_end]
+        #L3 = Return_only_complex(const_signal, 3)
+        L3, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR)  
+        push!(Third_ch, L3)
+    
+        Signal = const_signal[4][QRS_start:QRS_end]
+        #L4 = Return_only_complex(const_signal, 4)
+        L4, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Four_ch, L4)
+    
+        Signal = const_signal[5][QRS_start:QRS_end]
+        #L5 = Return_only_complex(const_signal, 5)
+        L5, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Five_ch, L5)
+    
+        Signal = const_signal[6][QRS_start:QRS_end]
+        #L6 = Return_only_complex(const_signal, 6)
+        L6, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Six_ch, L6)
+    
+        Signal = const_signal[7][QRS_start:QRS_end]
+        #L7 = Return_only_complex(const_signal, 7)
+        L7, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Seven_ch, L7)
+    
+        Signal = const_signal[8][QRS_start:QRS_end]
+        #L8 = Return_only_complex(const_signal, 8)
+        L8, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Eight_ch, L8)
+    
+        Signal = const_signal[9][QRS_start:QRS_end]
+        #L9 = Return_only_complex(const_signal, 9)
+        L9, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Nine_ch, L9)
+    
+        Signal = const_signal[10][QRS_start:QRS_end]
+        #L10 = Return_only_complex(const_signal, 10)
+        L10, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Ten_ch, L10)
+    
+        Signal = const_signal[11][QRS_start:QRS_end]
+        #L11 = Return_only_complex(const_signal, 11)
+        L11, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Eleven_ch, L11)
+    
+        Signal = const_signal[12][QRS_start:QRS_end]
+        #L12 = Return_only_complex(const_signal, 12)
+        L12, _ = dtw.Result_DTW(rd.Processing_Signal(Signal), K, Templates_Q, Templates_R, Templates_QR, Templates_QRS, Templates_RS, Templates_RSR) 
+        push!(Twelve_ch, L12)
+    
+        text = DataFrame(
+         name_File = Mass_Names_files,
+         _1 = First_ch,
+         _2 = Second_ch,
+         _3 = Third_ch,
+         _4 = Four_ch,
+         _5 = Five_ch,
+         _6 = Six_ch,
+         _7 = Seven_ch,
+         _8 = Eight_ch,
+         _9 = Nine_ch ,
+         _10 = Ten_ch,
+         _11 = Eleven_ch,
+         _12 = Twelve_ch)
+        CSV.write("Algorithm_DTW/scripts/Stats/$(name).csv", text, delim = ';')
+        end
+    end
+
+
+
+    export plot_templates, plots_result, Save_csv
 end
