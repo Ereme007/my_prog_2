c35394226cdfae7444de477295ecfe4a712266f3
Елизавета Александровна Еременко <eremenko.liz@mail.ru>
2024-07-09 15:20:58 +0300

    change Result_DTW() and DTW_kNN()

--- src/DTWfunc.jl
+++ src/DTWfunc.jl
@@ -3,54 +3,14 @@ module DTWfunc
     """
     Функция, определяющая принадлежность к классу (так же выводит классы, которые близки к исходному сигналу в порядке уменьшения схожести) 
     """
-    function Result_DTW(Signal,k, Q, R, QR, QRS, RS, RSR)
-        Result = DTW_kNN(Signal, k, Q, R, QR, QRS, RS, RSR)
-       
-        return Result[1][2], Result 
-    end
-    
-    function second_Result_DTW(Signal, k, Templates)
-        Result = second_DTW_kNN(Signal, k, Templates)
+    function Result_DTW(k, Signal, Templates)
+        Result = DTW_kNN(k, Signal, Templates)
    
         return Result[1][2], Result 
     end
-
-    #сделать набор шаблонов. Использрвать map()
-    #порядок шаблонов: Q, R, QR, QRS, RS, RSR. Сигнал сами передаём (с/без предобработки)
-    function DTW_kNN(Signal, k, Q, QR, QRS, RS, RSR, R)
-
-        temps = []
-        for i in 1:length(RS)
-            Templates = RS[i]
-            push!(temps, (dtw(Signal, Templates, SqEuclidean(); transportcost = 1)[1], "RS"))
-        end
-        for i in 1:length(RSR) 
-            Templates = RSR[i]
-            push!(temps, (dtw(Signal, Templates, SqEuclidean(); transportcost = 1)[1], "RSR"))
-        end
-        for i in 1:length(QR) 
-            Templates = QR[i]
-            push!(temps, (dtw(Signal, Templates, SqEuclidean(); transportcost = 1)[1], "QR"))
-        end
-        for i in 1:length(QRS) 
-            Templates = QRS[i]
-            push!(temps, (dtw(Signal, Templates, SqEuclidean(); transportcost = 1)[1], "QRS"))
-        end
-        for i in 1:length(R) 
-            Templates = R[i]
-            push!(temps, (dtw(Signal, Templates, SqEuclidean(); transportcost = 1)[1], "R"))
-        end
-        for i in 1:length(Q) 
-            Templates = Q[i]
-            push!(temps, (dtw(Signal, Templates, SqEuclidean(); transportcost = 1)[1], "Q"))
-        end
-        temps_sort = sort!(temps, by = x -> x[1]);
-
-        return temps_sort[1:k]
-    end
-
-    #сделать набор шаблонов. Использрвать map()
-    function second_DTW_kNN(Signal, k, Templates)
+    
+    #сделать набор шаблонов. С использрванием map()
+    function DTW_kNN(k, Signal, Templates)
         temps = []
         
         map(Templates) do fn
@@ -62,5 +22,5 @@ module DTWfunc
         return temps_sort[1:k]
     end
 
-    export Result_DTW, second_Result_DTW
+    export Result_DTW
 end
